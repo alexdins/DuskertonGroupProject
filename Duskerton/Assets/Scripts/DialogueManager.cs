0857e7e2b5cdb52e9333a8d6327410f77d08b0b6
Bakachloride <57642576+Bakachloride@users.noreply.github.com>
2021-04-14 04:48:57 -0400

    basic dialogue system(through code)

---
+++ Duskerton/Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DialogueManager : MonoBehaviour
+{
+
+    private Queue<string> sentences;
+    public GameObject TextBox;
+    public TextMeshProUGUI nameText;
+    public TextMeshProUGUI dialogueText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sentences = new Queue<string>();
+        
+
+    }
+
+    public void StartDialogue (Dialogue dialogue)
+    {
+        
+        nameText.SetText(dialogue.name);
+        TextBox.SetActive(true);
+        sentences.Clear();
+
+        foreach (string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+
+        }
+        DisplayNextSentence();
+
+    }
+
+    public void DisplayNextSentence()
+    {
+        if (sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+        string sentence = sentences.Dequeue();
+        dialogueText.SetText(sentence);
+    }
+
+    void EndDialogue()
+    {
+        TextBox.SetActive(false);
+        
+    }
+    
+}
---
+++ Duskerton/Assets/Scripts/DialogueTrigger.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour
+{
+    
+    public Dialogue dialogue;
+
+    public void TriggerDialogue()
+    {
+
+        FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+
+    }
+
+    public void OnTriggerStay(Collider other)
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (FindObjectOfType<DialogueManager>().TextBox.activeSelf)
+            {
+                FindObjectOfType<DialogueManager>().DisplayNextSentence();
+                return;
+            }
+            TriggerDialogue();
+        }
+    }
+
+}
