9a97ecfd9b14695eeb8c877b9371d019b3dabf26
caelstuffs <caelstuffs@gmail.com>
2021-04-21 05:42:18 -0400

    CameraSetup

---
+++ Duskerton/Assets/CameraCtrl.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraCtrl : MonoBehaviour
+{
+    public Camera[] views;
+    public int currCam;
+    
+    public float MouseSpeed;
+    float xRot = 0f;
+
+    void Start()
+    {
+        currCam = 0;
+        views[0].enabled = true;
+        views[1].enabled = false;
+
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            int setCam = 0;
+
+            if (currCam == 0) setCam = 1;
+            else if (currCam == 1) setCam = 0;
+            else Debug.LogError("currCam exceeds definitions!");
+
+            views[currCam].enabled = false;
+            views[setCam].enabled = true;
+            currCam = setCam;
+        }
+
+        if (views[1].enabled == true)
+        {
+            float mouseX = Input.GetAxis("Mouse X") * MouseSpeed * Time.deltaTime;
+            float mouseY = Input.GetAxis("Mouse Y") * MouseSpeed * Time.deltaTime;
+
+            xRot -= mouseY;
+            xRot = Mathf.Clamp(xRot, -90f, 45f);
+
+            views[1].transform.localRotation = Quaternion.Euler(xRot, 0f, 0f);
+            transform.Rotate(Vector3.up * mouseX);
+        }
+    }
+}
