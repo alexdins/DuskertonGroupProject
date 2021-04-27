8a3f29256f878edff9fc15d17e697215bc1a7a86
caelstuffs <caelstuffs@gmail.com>
2021-04-27 19:19:25 -0400

    Player rescaling and Camera Code rewriting

--- Duskerton/Assets/CameraCtrl.cs
+++ Duskerton/Assets/CameraCtrl.cs
@@ -4,17 +4,22 @@ using UnityEngine;
 
 public class CameraCtrl : MonoBehaviour
 {
+    public CharacterController control; 
     public Camera[] views;
     public int currCam;
-    
+
+    public float speed = 12f;
+    public float turnSmoothTime = 0.1f; 
     public float MouseSpeed;
+        
+    float turnSmoothVelocity;
     float xRot = 0f;
 
     void Start()
     {
         currCam = 0;
-        views[0].enabled = true;
-        views[1].enabled = false;
+        views[0].enabled = true; //Third Person View
+        views[1].enabled = false; //First Person View
 
         Cursor.lockState = CursorLockMode.Locked;
     }
@@ -22,29 +27,53 @@ public class CameraCtrl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float x = Input.GetAxisRaw("Horizontal");
+        float z = Input.GetAxisRaw("Vertical");
+
+        Vector3 move = new Vector3(x, 0f, z).normalized; 
+        
         if (Input.GetKeyDown("space"))
-        {
-            int setCam = 0;
-
-            if (currCam == 0) setCam = 1;
-            else if (currCam == 1) setCam = 0;
-            else Debug.LogError("currCam exceeds definitions!");
-
-            views[currCam].enabled = false;
-            views[setCam].enabled = true;
-            currCam = setCam;
-        }
+            SetCamera();
 
         if (views[1].enabled == true)
+            FpsView();
+        
+        if (move.magnitude >= 0.1f)
         {
-            float mouseX = Input.GetAxis("Mouse X") * MouseSpeed * Time.deltaTime;
-            float mouseY = Input.GetAxis("Mouse Y") * MouseSpeed * Time.deltaTime;
+            Transform cam = views[currCam].transform;
+            
+            float targetAngle = Mathf.Atan2(move.x, move.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
-            xRot -= mouseY;
-            xRot = Mathf.Clamp(xRot, -90f, 45f);
-
-            views[1].transform.localRotation = Quaternion.Euler(xRot, 0f, 0f);
-            transform.Rotate(Vector3.up * mouseX);
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            control.Move(moveDir * speed * Time.deltaTime);
         }
+
+    }
+
+    void SetCamera() 
+    {
+        int setCam = 0;
+
+        if (currCam == 0) setCam = 1;
+        else if (currCam == 1) setCam = 0;
+        else Debug.LogError("currCam exceeds definitions!");
+
+        views[currCam].enabled = false;
+        views[setCam].enabled = true;
+        currCam = setCam;
+    }
+
+    void FpsView()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * MouseSpeed * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * MouseSpeed * Time.deltaTime;
+
+        xRot -= mouseY;
+        xRot = Mathf.Clamp(xRot, -90f, 45f);
+
+        views[1].transform.localRotation = Quaternion.Euler(xRot, 0f, 0f);
+        transform.Rotate(Vector3.up * mouseX);
     }
 }
--- Duskerton/Assets/PlayerCtrl.cs
+++ Duskerton/Assets/PlayerCtrl.cs
@@ -5,21 +5,33 @@ using UnityEngine;
 public class PlayerCtrl : MonoBehaviour
 {
     public CharacterController control;
+    public Camera thirdCam;
+    
     public float speed = 12f;
-
-    void Start()
-    {
-
-    }
-
+    public float turnSmoothTime = 0.1f;
+    float turnSmoothVelocity;
     // Update is called once per frame
     void Update()
     {
-        float x = Input.GetAxis("Horizontal");
-        float z = Input.GetAxis("Vertical");
+        float x = Input.GetAxisRaw("Horizontal");
+        float z = Input.GetAxisRaw("Vertical");
 
-        Vector3 move = transform.right * x + transform.forward * z;
+        Vector3 move = new Vector3(x, 0f, z).normalized;
 
-        control.Move(move * speed * Time.deltaTime);
+        if (move.magnitude >= 0.1f && thirdCam.enabled)
+        {
+            float moveAngle = Mathf.Atan2(move.x, move.y) * Mathf.Rad2Deg + thirdCam.transform.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, moveAngle, ref turnSmoothVelocity, turnSmoothTime
+                );
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            Vector3 moveDir = Quaternion.Euler(0f, moveAngle, 0f) * Vector3.forward;
+            control.Move(moveDir * speed * Time.deltaTime);
+        }
+
+        else if (move.magnitude >= 0.1f)
+{
+            control.Move(move * speed * Time.deltaTime);
+        }
     }
 }
