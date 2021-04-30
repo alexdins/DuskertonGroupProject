2d80c3481af18269087cb881e3b11dfdb10fb351
caelstuffs <caelstuffs@gmail.com>
2021-04-30 17:16:14 -0400

    Camera Code, Code Organizing and Animation Controls

---
+++ Duskerton/Assets/Scripts/Alice Scripts/MoveCtrl.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCtrl : MonoBehaviour
+{
+    public CameraCtrl CameraCtrl;
+    public float turnSmoothTime = 0.1f;
+    public float speed = 12f;
+
+    float turnSmoothVelocity;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float z = Input.GetAxisRaw("Vertical");
+
+        Vector3 move = new Vector3(x, 0f, z).normalized;
+
+        if (move.magnitude >= 0.1f)
+        {
+            Transform cam = CameraCtrl.views[CameraCtrl.currCam].transform;
+            float targetAngle = Mathf.Atan2(move.x, move.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+
+            if (CameraCtrl.currCam == 0) transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            CameraCtrl.control.Move(moveDir * speed * Time.deltaTime);
+
+        }
+    }
+}
